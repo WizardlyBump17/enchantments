00b6e89c3929c297549b21bff25a5c43df08530c
WizardlyBump17 <davicraft130@gmail.com>
2024-05-15 16:23:17 -0300

    added the PaperSupport class

---
+++ paper/src/main/java/com/wizardlybump17/enchantments/paper/PaperSupport.java
@@ -0,0 +1,13 @@
+package com.wizardlybump17.enchantments.paper;
+
+import com.wizardlybump17.enchantments.api.loader.EnchantmentLoader;
+import com.wizardlybump17.enchantments.paper.loader.PaperEnchantmentLoader;
+import lombok.experimental.UtilityClass;
+
+@UtilityClass
+public class PaperSupport {
+
+    public static void start() {
+        EnchantmentLoader.setInstance(new PaperEnchantmentLoader());
+    }
+}
