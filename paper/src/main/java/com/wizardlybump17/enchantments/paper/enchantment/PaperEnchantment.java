befaadbd7a35939e8e8e4a17f53094daa1d3c33d
WizardlyBump17 <davicraft130@gmail.com>
2024-05-15 14:46:58 -0300

    added the PaperEnchantment class

---
+++ paper/src/main/java/com/wizardlybump17/enchantments/paper/enchantment/PaperEnchantment.java
@@ -0,0 +1,28 @@
+package com.wizardlybump17.enchantments.paper.enchantment;
+
+import com.wizardlybump17.enchantments.api.enchantment.Enchantment;
+import com.wizardlybump17.enchantments.api.id.Id;
+import com.wizardlybump17.enchantments.api.listener.EnchantmentListener;
+import lombok.NonNull;
+import org.bukkit.configuration.serialization.ConfigurationSerializable;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+
+public abstract class PaperEnchantment extends Enchantment implements ConfigurationSerializable {
+
+    public PaperEnchantment(@NonNull Id id, String name, int maxLevel, @NonNull Map<Object, List<EnchantmentListener>> listeners) {
+        super(id, name, maxLevel, listeners);
+    }
+
+    @Override
+    public @NotNull Map<String, Object> serialize() {
+        Map<String, Object> map = new LinkedHashMap<>();
+        map.put("id", getId().toString());
+        map.put("name", getName());
+        map.put("max-level", getMaxLevel());
+        return map;
+    }
+}
