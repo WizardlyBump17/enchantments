b98b9bb33907915eafe75d996607d82bc60b771a
WizardlyBump17 <davicraft130@gmail.com>
2024-05-15 13:13:13 -0300

    added the maven-publish plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,8 @@
 plugins {
     id("com.github.johnrengelman.shadow") version "8.1.1"
     id("java")
+    id("maven-publish")
+    id("io.freefair.lombok") version "8.6" apply false
 }
 
 allprojects {
@@ -11,6 +13,9 @@ allprojects {
 }
 
 subprojects {
+    apply(plugin = "io.freefair.lombok")
+    apply(plugin = "maven-publish")
+
     repositories {
         mavenCentral()
         maven {
@@ -26,6 +31,28 @@ subprojects {
         compileJava {
             options.encoding = Charsets.UTF_8.name()
             options.release.set(16)
+        }
+
+        java {
+            withJavadocJar()
+            withSourcesJar()
+        }
+
+        javadoc {
+            dependsOn(getTasksByName("delombok", false))
+            options {
+                this as StandardJavadocDocletOptions
+                addBooleanOption("Xdoclint:none", true)
+                addStringOption("Xmaxwarns", "1")
+            }
+        }
+
+        publishing {
+            publications {
+                create<MavenPublication>("maven") {
+                    from(project.components["java"])
+                }
+            }
         }
     }
 }
