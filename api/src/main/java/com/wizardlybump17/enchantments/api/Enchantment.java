3836b967930a927a5679d79f4f23c378e0ab0c7b
WizardlyBump17 <davicraft130@gmail.com>
2024-05-14 23:39:45 -0300

    improved the builder methods

--- api/src/main/java/com/wizardlybump17/enchantments/api/Enchantment.java
+++ api/src/main/java/com/wizardlybump17/enchantments/api/Enchantment.java
@@ -3,6 +3,7 @@ package com.wizardlybump17.enchantments.api;
 import com.wizardlybump17.enchantments.api.id.Id;
 import com.wizardlybump17.enchantments.api.id.Identifiable;
 import com.wizardlybump17.enchantments.api.listener.EnchantmentListener;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NonNull;
 import lombok.experimental.SuperBuilder;
@@ -16,7 +17,7 @@ public abstract class Enchantment implements Identifiable {
     private final @NonNull Id id;
     private final String name;
     private final int maxLevel;
-    private final @NonNull Map<Object, List<EnchantmentListener>> listeners = new HashMap<>();
+    private final @NonNull @Builder.Default Map<Object, List<EnchantmentListener>> listeners = new HashMap<>();
 
     public void addListener(@NonNull EnchantmentListener listener) {
         if (listener.register(this))
@@ -38,5 +39,21 @@ public abstract class Enchantment implements Identifiable {
     public void clearListeners(@NonNull Object key) {
         for (EnchantmentListener listener : listeners.remove(key))
             listener.unregister(this);
+    }
+
+    public abstract static class EnchantmentBuilder {
+
+        public @NonNull EnchantmentBuilder addListener(@NonNull EnchantmentListener listener) {
+            listeners$value.put(listener.getKey(), Collections.singletonList(listener));
+            return this;
+        }
+
+        public @NonNull EnchantmentBuilder removeListener(@NonNull EnchantmentListener listener) {
+            listeners$value.computeIfPresent(listener.getKey(), (key, listeners) -> {
+                listeners.remove(listener);
+                return listeners;
+            });
+            return this;
+        }
     }
 }
