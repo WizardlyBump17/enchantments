490d69eff6ea4d3f91261f72dd0357f912c04cf2
WizardlyBump17 <davicraft130@gmail.com>
2024-05-15 16:19:42 -0300

    added the EnchantmentLoader class

---
+++ api/src/main/java/com/wizardlybump17/enchantments/api/loader/EnchantmentLoader.java
@@ -0,0 +1,44 @@
+package com.wizardlybump17.enchantments.api.loader;
+
+import com.wizardlybump17.enchantments.api.enchantment.Enchantment;
+import lombok.NonNull;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Optional;
+
+public abstract class EnchantmentLoader {
+
+    private static EnchantmentLoader instance;
+
+    public static void setInstance(@NonNull EnchantmentLoader instance) {
+        if (EnchantmentLoader.instance != null)
+            throw new IllegalStateException("The instance is already set");
+        EnchantmentLoader.instance = instance;
+    }
+
+    public static @NonNull EnchantmentLoader getInstance() {
+        return instance;
+    }
+
+    public abstract @NonNull Optional<Enchantment> load(@NonNull File file);
+
+    public @NonNull List<Enchantment> loadAll(@NonNull File folder) {
+        File[] files = folder.listFiles();
+        if (files == null)
+            return Collections.emptyList();
+
+        List<Enchantment> enchantments = new ArrayList<>();
+        for (File file : files) {
+            Optional<Enchantment> optional = load(file);
+            optional.ifPresent(enchantment -> {
+                if (enchantment.register())
+                    enchantments.add(enchantment);
+            });
+        }
+
+        return enchantments;
+    }
+}
