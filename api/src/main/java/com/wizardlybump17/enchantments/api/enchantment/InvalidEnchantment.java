fc807b146cd15af2be9c2c220224d83f15dbcd60
WizardlyBump17 <davicraft130@gmail.com>
2024-05-14 23:14:11 -0300

    added the InvalidEnchantment class

---
+++ api/src/main/java/com/wizardlybump17/enchantments/api/enchantment/InvalidEnchantment.java
@@ -0,0 +1,14 @@
+package com.wizardlybump17.enchantments.api.enchantment;
+
+import com.wizardlybump17.enchantments.api.Enchantment;
+import com.wizardlybump17.enchantments.api.id.Id;
+import lombok.NonNull;
+
+import java.util.Collections;
+
+public final class InvalidEnchantment extends Enchantment {
+
+    public InvalidEnchantment(@NonNull Id id) {
+        super(id, Collections.emptyMap());
+    }
+}
