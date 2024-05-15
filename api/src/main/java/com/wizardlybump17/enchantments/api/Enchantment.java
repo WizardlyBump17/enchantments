2c17e6d0be8f14d9d67ba79b8900e08a75e8754e
WizardlyBump17 <davicraft130@gmail.com>
2024-05-14 22:54:02 -0300

    added the EnchantmentListener system

--- api/src/main/java/com/wizardlybump17/enchantments/api/Enchantment.java
+++ api/src/main/java/com/wizardlybump17/enchantments/api/Enchantment.java
@@ -2,8 +2,14 @@ package com.wizardlybump17.enchantments.api;
 
 import com.wizardlybump17.enchantments.api.id.Id;
 import com.wizardlybump17.enchantments.api.id.Identifiable;
+import com.wizardlybump17.enchantments.api.listener.EnchantmentListener;
 import lombok.Data;
 import lombok.NonNull;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
 
 @Data
 public abstract class Enchantment implements Identifiable {
@@ -11,4 +17,24 @@ public abstract class Enchantment implements Identifiable {
     private final @NonNull Id id;
     private String name;
     private int maxLevel;
+    private final @NonNull Map<Object, List<EnchantmentListener>> listeners;
+
+    public void addListener(@NonNull EnchantmentListener listener) {
+        listeners.computeIfAbsent(listener.getKey(), $ -> new ArrayList<>()).add(listener);
+    }
+
+    public void removeListener(@NonNull EnchantmentListener listener) {
+        this.listeners.computeIfPresent(listener.getKey(), (key, listeners) -> {
+            listeners.remove(listener);
+            return listeners;
+        });
+    }
+
+    public boolean hasListener(@NonNull EnchantmentListener listener) {
+        return listeners.computeIfAbsent(listener, $ -> Collections.emptyList()).contains(listener);
+    }
+
+    public void clearListeners(@NonNull Object key) {
+        listeners.remove(key);
+    }
 }
---
+++ api/src/main/java/com/wizardlybump17/enchantments/api/listener/EnchantmentListener.java
@@ -0,0 +1,9 @@
+package com.wizardlybump17.enchantments.api.listener;
+
+import lombok.NonNull;
+
+public interface EnchantmentListener {
+
+    @NonNull
+    Object getKey();
+}
