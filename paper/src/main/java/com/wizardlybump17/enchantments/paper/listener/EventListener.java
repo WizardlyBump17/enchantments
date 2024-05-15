d57471cee1f2757be9234cc15d8476465275c41c
WizardlyBump17 <davicraft130@gmail.com>
2024-05-14 23:28:51 -0300

    added the EventListener class

---
+++ paper/src/main/java/com/wizardlybump17/enchantments/paper/listener/EventListener.java
@@ -0,0 +1,40 @@
+package com.wizardlybump17.enchantments.paper.listener;
+
+import com.wizardlybump17.enchantments.api.listener.EnchantmentListener;
+import lombok.Builder;
+import lombok.NonNull;
+import org.bukkit.Bukkit;
+import org.bukkit.event.Event;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.HandlerList;
+import org.bukkit.event.Listener;
+import org.bukkit.plugin.Plugin;
+
+import java.util.function.Consumer;
+
+@Builder
+public class EventListener<E extends Event> implements EnchantmentListener, Listener {
+
+    private final @NonNull Class<E> eventClass;
+    private final @NonNull Plugin plugin;
+    private final @NonNull Consumer<E> executor;
+    private final @NonNull @Builder.Default EventPriority priority = EventPriority.NORMAL;
+    private final boolean ignoreCancelled;
+
+    @Override
+    public @NonNull Object getKey() {
+        return eventClass;
+    }
+
+    @SuppressWarnings("unchecked")
+    @Override
+    public boolean register() {
+        Bukkit.getPluginManager().registerEvent(eventClass, this, priority, (listener, event) -> executor.accept((E) event), plugin, ignoreCancelled);
+        return true;
+    }
+
+    @Override
+    public void unregister() {
+        HandlerList.unregisterAll(this);
+    }
+}
